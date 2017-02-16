702d6a3ab519c3489398292fb06f1cfd1f5c44dc
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-02-16 22:10:55 +0100

    implement chessboard grains

--- src/grains/core.clj
+++ src/grains/core.clj
@@ -1,7 +1,12 @@
 (ns grains.core)
 
-(defn- exp [x n]
-  (reduce * (repeat n x)))
+(defn exp [x n]
+  "x^n, uses *' to avoid ArithmeticException"
+  (reduce *' (repeat n x)))
 
 (defn square->grains [square]
   (exp 2 (- square 1)))
+
+(defn chessboard->grains []
+  (apply + (map square->grains (range 1 (inc 64)))))
+
--- test/grains/core_test.clj
+++ test/grains/core_test.clj
@@ -2,8 +2,17 @@
   (:require [clojure.test :refer :all]
             [grains.core :refer :all]))
 
+(defn- geometric-progression->sum [a r n]
+  "geometric progression sum formulae from https://en.wikipedia.org/wiki/Geometric_progression#Derivation"
+  (* a (/ (- 1 (exp r n)) (- 1 r))))
+
 (deftest grains-test
+
   (testing "grains per square"
     (is (= 1 (square->grains 1)))
     (is (= 2 (square->grains 2)))
-    (is (= 4 (square->grains 3)))))
+    (is (= 4 (square->grains 3)))
+    (is (= 2 (/ (square->grains 64) (square->grains 63)))))
+
+  (testing "chessboard grains"
+    (is (= (geometric-progression->sum 1 2 64) (chessboard->grains)))))
